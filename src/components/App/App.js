7d61f5004bcc27ac890029039699c9e7ac80a8a5
Vova Pol <ya-chai777@yandex.ru>
2023-10-16 18:32:25 +0300

    Задана базовая структура

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import { Footer } from '../Footer/Footer';
+import { Header } from '../Header/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main></main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
