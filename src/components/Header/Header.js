bb7d9d5c62641a1ad0f5415c91dc1840edb1ebf1
Vova Pol <ya-chai777@yandex.ru>
2023-10-26 17:13:29 +0300

    Добавлена базовая верста и функционал в компонент Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { HashLink as Link } from 'react-router-hash-link';
 import './Header.css';
+import { LiaLaptopCodeSolid } from 'react-icons/lia';
+import { FaLightbulb, FaRegLightbulb } from 'react-icons/fa6';
 
 export const Header = () => {
-  return <div>Header</div>;
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  function handleChangeTheme() {
+    setIsDarkTheme(!isDarkTheme);
+  }
+
+  return (
+    <header className="header">
+      <nav className="header__nav">
+        <ul className="header__list">
+          <li>
+            <Link to="/" className="header__link">
+              <LiaLaptopCodeSolid className="header__logo" />
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="header__link">
+              Главная
+            </Link>
+          </li>
+          <li>
+            <Link to="/#skills" className="header__link">
+              Скилы
+            </Link>
+          </li>
+          <li>
+            <Link to="/#projects" className="header__link">
+              Проекты
+            </Link>
+          </li>
+          <li>
+            <Link to="/#movie-explorer" className="header__link">
+              Movies Expolrer
+            </Link>
+          </li>
+          <li>
+            <Link to="/#traveler" className="header__link">
+              Traveler
+            </Link>
+          </li>
+          <li>
+            <Link to="/#contacts" className="header__link">
+              Контакты
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="header__input-container">
+        {/* <input
+          className="header__input"
+          type="checkbox"
+          checked={isDarkTheme}
+          onChange={handleChangeTheme}
+        ></input> */}
+        <span onClick={handleChangeTheme}>
+          {isDarkTheme ? (
+            <FaLightbulb className="header__theme-icon" />
+          ) : (
+            <FaRegLightbulb className="header__theme-icon" />
+          )}
+        </span>
+      </div>
+    </header>
+  );
 };
