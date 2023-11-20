fe993413cc45dda1f992a96d4d7fb9a02ace3abe
Vova Pol <ya-chai777@yandex.ru>
2023-11-20 11:23:31 +0300

    Доработан компонент Contacts

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,6 +1,41 @@
 import React from 'react';
 import './Contacts.css';
+import { ReactComponent as EmailIcon } from '../../images/contacts-mail.svg';
 
 export const Contacts = () => {
-  return <section id="contacts" className="contacts"></section>;
+  return (
+    <section id="contacts" className="contacts">
+      <div className="contacts__container">
+        <div className="contacts__icon-container">
+          <EmailIcon className="contacts-email-icon" />
+        </div>
+        <form className="contacts__form">
+          <h3 className="contacts__form-title">Напиши мне</h3>
+          <label className="contacts__label">От кого:</label>
+          <input
+            type="text"
+            className="contacts__input"
+            placeholder="ФИО"
+            required
+          ></input>
+          <label className="contacts__label">E-mail:</label>
+          <input
+            type="email"
+            className="contacts__input"
+            placeholder="example@gmail.com"
+            required
+          ></input>
+          <label className="contacts__label">Сообщение:</label>
+          <textarea
+            className="contacts__textarea"
+            placeholder="Привет, Вов!"
+            required
+          ></textarea>
+          <button type="submit" className="contacts__button">
+            Отправить
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
