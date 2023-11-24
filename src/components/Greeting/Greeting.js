34a1ca3e0e11e69eae191e7bad9628632f551506
Vova Pol <ya-chai777@yandex.ru>
2023-11-24 16:04:24 +0300

    Добавлена возможность скачать CV

--- src/components/Greeting/Greeting.js
+++ src/components/Greeting/Greeting.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Greeting.css';
 import { Link } from 'react-router-dom';
 import { GITHUB_URL, LEETCODE_URL } from '../../utils/constants';
 import { SiGithub, SiLeetcode } from 'react-icons/si';
 import { MdDownload } from 'react-icons/md';
+import { CiBookmarkCheck } from 'react-icons/ci';
 const avatar = require('../../images/avatar.jpg');
 
 export const Greeting = () => {
+  const [isDownloaded, setIsDownloaded] = useState(false);
   return (
     <section className="greeting">
       <img src={avatar} alt="аватар" className="greeting__avatar"></img>
@@ -27,13 +29,31 @@ export const Greeting = () => {
           проектом и развиваться как разработчик.
         </p>
         <div className="greeting__buttons-container">
-          <Link to={GITHUB_URL} className="greeting__link" target="_blank">
+          <Link
+            to={GITHUB_URL}
+            className="greeting__link greeting__link_type_github"
+            target="_blank"
+          >
             <SiGithub /> GitHub
           </Link>
-          <button className="greeting__button">
+          <Link
+            to="/test-cv.pdf"
+            className="greeting__link greeting__link_type_cv"
+            target="_blank"
+            download
+            onClick={() => setIsDownloaded(true)}
+          >
             <MdDownload />
             Скачать CV
-          </button>
+          </Link>
+
+          <CiBookmarkCheck
+            className={
+              isDownloaded
+                ? 'greeting__success-icon greeting__success-icon_shown'
+                : 'greeting__success-icon'
+            }
+          />
         </div>
       </article>
     </section>
